16a922f99176680802de0b198f35d817b9c115f4
gaetanoquarto <gaetanoqquarto@gmail.com>
2023-02-27 11:00:21 +0100

    creazione classi indirizzo, provincia, comune

--- src/main/java/it/epicode/gruppo1/app/entities/Comune.java
+++ src/main/java/it/epicode/gruppo1/app/entities/Comune.java
@@ -1,5 +1,36 @@
 package it.epicode.gruppo1.app.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "comuni")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class Comune {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String nome;
+	
+	@ManyToOne
+	private Provincia provincia;
 
 }
--- src/main/java/it/epicode/gruppo1/app/entities/Provincia.java
+++ src/main/java/it/epicode/gruppo1/app/entities/Provincia.java
@@ -1,5 +1,33 @@
 package it.epicode.gruppo1.app.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "province")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class Provincia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String nome;
+	private String sigla;
 
 }
