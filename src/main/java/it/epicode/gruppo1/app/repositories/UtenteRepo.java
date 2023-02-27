5b242fc4b0b87a130bf242bf5a9e2d708f3c4051
Flavio Cirillo <scatto2003@gmail.com>
2023-02-27 11:41:39 +0100

    Creazione Service e Repository
    Creazione service e repository delle classi Ruolo e Utente

--- src/main/java/it/epicode/gruppo1/app/repositories/UtenteRepo.java
+++ src/main/java/it/epicode/gruppo1/app/repositories/UtenteRepo.java
@@ -1,5 +1,11 @@
 package it.epicode.gruppo1.app.repositories;
 
-public interface UtenteRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.epicode.gruppo1.app.entities.Utente;
+
+@Repository
+public interface UtenteRepo extends JpaRepository<Utente, Integer> {
 
 }
