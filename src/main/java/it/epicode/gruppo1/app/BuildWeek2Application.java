025f378ac9cc6bf9f243284e1cc52fe61bf2d1b2
gaetanoquarto <gaetanoqquarto@gmail.com>
2023-02-27 09:58:19 +0100

    project creation

---
+++ src/main/java/it/epicode/gruppo1/app/BuildWeek2Application.java
@@ -0,0 +1,13 @@
+package it.epicode.gruppo1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeek2Application.class, args);
+	}
+
+}
