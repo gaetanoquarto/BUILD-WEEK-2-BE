c645aeae034239f40edf43cd06b9b3a3be4d4a5c
SaraCirone <114257693+SaraCirone@users.noreply.github.com>
2023-02-27 10:42:22 +0100

    creazione base classe fattura

--- src/main/java/it/epicode/gruppo1/app/entities/Fattura.java
+++ src/main/java/it/epicode/gruppo1/app/entities/Fattura.java
@@ -1,5 +1,41 @@
 package it.epicode.gruppo1.app.entities;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "fatture")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+
 public class Fattura {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	
+	private int anno; 
+	private LocalDate data;
+	private double importo; 
+	private int numero;
+
+	
+		
 }
--- src/main/java/it/epicode/gruppo1/app/entities/enums/StatoFattura.java
+++ src/main/java/it/epicode/gruppo1/app/entities/enums/StatoFattura.java
@@ -2,4 +2,7 @@ package it.epicode.gruppo1.app.entities.enums;
 
 public enum StatoFattura {
 
+	Pagata, 
+	Da_pagare, 
+	Scaduta
 }
