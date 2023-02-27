5d202d10d34b0098343def56303be72feb3b7b40
Flavio Cirillo <scatto2003@gmail.com>
2023-02-27 10:47:54 +0100

    Update Utente.java
    primo commit

--- src/main/java/it/epicode/gruppo1/app/entities/Utente.java
+++ src/main/java/it/epicode/gruppo1/app/entities/Utente.java
@@ -1,5 +1,52 @@
 package it.epicode.gruppo1.app.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Utente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	private String email;
+	private String password;
+	private String nome;
+	private String cognome;
+	private boolean attivo;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "ruoli_utenti",
+			joinColumns = @JoinColumn(name = "utente_id"),
+			inverseJoinColumns = @JoinColumn(name = "ruolo_id")
+			)
+	private Set<Ruolo> ruoli = new HashSet<>();
+	
 }
